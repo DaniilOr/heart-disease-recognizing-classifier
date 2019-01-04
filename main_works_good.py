4b3a2d08786c4227ea912784adc1379679e8fbba
Daniil <36066254+DaniilOr@users.noreply.github.com>
2019-01-04 22:16:11 +0600

    Add files via upload

---
+++ main_works_good.py
@@ -0,0 +1,74 @@
+﻿import sys  
+import os  
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from PyQt5 import QtWidgets
+import pickle
+import pds as projectDesign 
+from sklearn.externals import joblib
+from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem
+import csv
+def make_str(array):
+	answer_str=''
+	for i in range(len(array)):
+		diagnoz=''
+		if(array[i]==0):
+			diagnoz = 'Здоров'
+		if(array[i]==1):
+			diagnoz='Слабые подозрение на заболевание сердца'
+		if(array[i]==2):
+			diagnoz='Возможна болезнь сердца'
+		if(array[i]==3):
+			diagnoz='Обнаружена болезнь сердца'
+		if(array[i]==4):
+			diagnoz='Серьезное заболевание сердца'
+		
+		answer_str+=('У '+str(i+1)+'ого пациента диагноз - '+str(diagnoz)+'\n')
+	return answer_str
+class ExampleApp(QtWidgets.QMainWindow, projectDesign.Ui_MainWindow):
+    def __init__(self):
+          super().__init__()
+          self.setupUi(self)
+          self.pushButton.clicked.connect(self.browse_folder)  
+
+
+    def browse_folder(self):
+        #self.listWidget.clear() 
+        directory = QtWidgets.QFileDialog.getOpenFileName(self, "Select a folder")
+        data=pd.read_csv(directory[0].split('/')[-1])
+        
+
+        if directory: 
+            rfc = joblib.load('rf_classifier.pkl')
+            ic=0
+            self.tableWidget.setColumnCount(13)
+            with open(directory[0].split('/')[-1], newline='') as csvfile:
+                reader=csv.reader(csvfile,delimiter=',')
+                for row in reader:
+                    ic+=1
+            self.tableWidget.setRowCount(ic)
+            ic=0
+            with open('test.csv', newline='') as csvfile:
+                reader=csv.reader(csvfile,delimiter=',')
+            #print(reader)
+			
+                for row in reader:
+                    
+                    for j in range(len(row)):
+                    
+                        self.tableWidget.setItem(ic,j, QTableWidgetItem(row[j]))
+                    ic+=1
+                    
+            mb=QMessageBox();predictions=rfc.predict(data)
+            mb.information(self, "Prediction", make_str(rfc.predict(data))); self.tableWidget.show()
+
+			
+  
+def main():
+	
+    app = QtWidgets.QApplication(sys.argv)  
+    window = ExampleApp()  
+    window.show() 
+    app.exec_()  
+if __name__ == '__main__':  
+    main()  
---
+++ pds.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'projectDesign.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(242, 295)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
+        self.verticalLayout.addWidget(self.tableWidget)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 242, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Open database"))
+
